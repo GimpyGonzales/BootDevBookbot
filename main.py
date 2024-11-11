e6bd1836019766020ad3ef1845f655f765c4851e
GimpyGonzales <jhutchings317@gmail.com>
2024-11-11 13:58:52 -0500

    Updated bookbot to count da letters. Can make the file reading its own function though. Will adjust for next assignment.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def main():
     bookPath = "books/frankenstein.txt"
-    text = printText(bookPath)
-    countWords = wordCount(bookPath)
-    print(f"{countWords} words found in document in path {bookPath}")
+    letterCount = characterCount(bookPath)
+    print(f"Please see below for count of all letters in file in path {bookPath}")
+    print(letterCount)
 
 def printText(path):
     with open(path) as f:
@@ -16,4 +16,17 @@ def wordCount(path):
         wordCount = len(words)
         return wordCount
 
+def characterCount(path):
+    charCountDict = {}
+    with open(path) as f:
+        fileContents = f.read()
+        for letter in fileContents:
+            if letter.isalpha():
+                letter = letter.lower()
+                if letter in charCountDict:
+                    charCountDict[letter] += 1
+                else:
+                    charCountDict[letter] = 1
+        return charCountDict
+    
 main()
