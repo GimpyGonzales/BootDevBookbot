8f2eb640894cd08a26f3a43857a6512c8d8fd639
GimpyGonzales <jhutchings317@gmail.com>
2024-11-11 14:44:17 -0500

    Finished the Bookbot! It counts total number of words, letters, and alphabetizes the results.

--- main.py
+++ main.py
@@ -1,32 +1,53 @@
 def main():
     bookPath = "books/frankenstein.txt"
-    letterCount = characterCount(bookPath)
-    print(f"Please see below for count of all letters in file in path {bookPath}")
-    print(letterCount)
+    text = getText(bookPath)
+    numWords = wordCount(text)
+    letterCount = characterCount(text)
+    numLetters = countedLetters(letterCount)
+    niceReport = prettyReport(letterCount)
+    print("*-*-* BOOT.DEV BOOK BOT REPORT *-*-*")
+    print(f"Path to file chosen: {bookPath}")
+    print(f"Total number of words in {bookPath}: {numWords}")
+    print(f"Total number of letters in {bookPath}: {numLetters}")
+    for line in niceReport:
+        print(line)
 
-def printText(path):
-    with open(path) as f:
-        fileContents = f.read()
-        return fileContents
 
-def wordCount(path):
-    with open(path) as f:
-        fileContents = f.read()
-        words = fileContents.split()
-        wordCount = len(words)
-        return wordCount
+def getText(path):
+    with open(path) as j:
+        return j.read()
 
-def characterCount(path):
-    charCountDict = {}
-    with open(path) as f:
-        fileContents = f.read()
-        for letter in fileContents:
-            if letter.isalpha():
-                letter = letter.lower()
-                if letter in charCountDict:
-                    charCountDict[letter] += 1
-                else:
-                    charCountDict[letter] = 1
-        return charCountDict
+
+def wordCount(file):
+    words = file.split()
+    wordCount = len(words)
+    return wordCount
+
+def countedLetters(charList):
+    totalLetters = 0
+    for c in charList:
+        totalLetters += charList[c]
+    return totalLetters
     
+
+def characterCount(file):
+    charCountDict = {}
+    for letter in file:
+        if letter.isalpha():
+            letter = letter.lower()
+            if letter in charCountDict:
+                charCountDict[letter] += 1
+            else:
+                charCountDict[letter] = 1
+    return charCountDict
+
+
+def prettyReport(charCount):
+    reportList = []
+    sortLetters = dict(sorted(charCount.items()))
+    for c in sortLetters:
+        reportList.append(f"The letter {c} appeared in this text {sortLetters[c]} times.")
+    return reportList 
+
+
 main()
